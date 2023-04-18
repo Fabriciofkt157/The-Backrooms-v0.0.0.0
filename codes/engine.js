5dfceca516d58f8433f3e6e82048a18ec026d6b7
Fabriciofkt157 <99661346+Fabriciofkt157@users.noreply.github.com>
2023-04-17 23:22:21 -0300

    Update engine.js

--- codes/engine.js
+++ codes/engine.js
@@ -1,51 +1,25 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+// Crie um objeto de configuração para o jogo
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: [MenuPrincipal]
+};
 
-// Define uma geometria e um material para um cubo
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// Crie um objeto de jogo
+var game = new Phaser.Game(config);
 
-// Define a posição inicial da câmera
-camera.position.set( 0, 1.5, 0 );
+class MenuPrincipal extends Phaser.Scene {
+  function preload() {
+    this.load.image('bg', 'assets/bg.png');
+  }
 
-// Define a rotação inicial da câmera
-camera.rotation.order = "YXZ";
-camera.rotation.y = Math.PI;
-camera.rotation.x = -Math.PI / 6;
+  function create() {
+    this.add.image(0, 0, 'bg').setOrigin(0, 0);
+  }
 
-// Adiciona a câmera à cena
-scene.add( camera );
-
-// Função para atualizar a posição da câmera com base no toque do usuário
-function onTouchMove( event ) {
-  // Obtém a diferença de posição do toque anterior para o atual
-  var deltaX = event.touches[ 0 ].clientX - previousTouchX;
-  var deltaY = event.touches[ 0 ].clientY - previousTouchY;
-
-  // Atualiza a rotação da câmera com base na diferença de posição
-  camera.rotation.y -= deltaX * 0.01;
-  camera.rotation.x -= deltaY * 0.01;
-
-  // Limita a rotação vertical da câmera
-  camera.rotation.x = Math.max( -Math.PI / 2, Math.min( Math.PI / 2, camera.rotation.x ) );
-
-  // Salva a posição do toque para o próximo frame
-  previousTouchX = event.touches[ 0 ].clientX;
-  previousTouchY = event.touches[ 0 ].clientY;
+  function update() {
+    // Atualize objetos
+  }
+  
 }
-
-// Adiciona o listener de toque para a câmera
-var previousTouchX, previousTouchY;
-renderer.domElement.addEventListener( 'touchstart', function( event ) {
-  previousTouchX = event.touches[ 0 ].clientX;
-  previousTouchY = event.touches[ 0 ].clientY;
-  renderer.domElement.addEventListener( 'touchmove', onTouchMove, false );
-}, false );
-renderer.domElement.addEventListener( 'touchend', function() {
-  renderer.domElement.removeEventListener( 'touchmove', onTouchMove, false );
-}, false );
